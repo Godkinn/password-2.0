75c32a86fa7b3f3da0c91d9bb3c919cb744bce9e
Godkinn <dastiki@outlook.com>
2016-08-21 21:59:13 +0200

    Update main.java

--- JAVAversion/V5.0/main.java
+++ JAVAversion/V5.0/main.java
@@ -1,1 +1,43 @@
 import util.java.Scanner;
+
+class main {
+  //A variable I am going to use in both functions, so I have to declare it outside both
+  static int PassLeft = 1;
+  
+  public static void main(String[] args) {
+    //Variables I am going to use
+    int Num = 0;
+    int TriesLeft = 1;
+    
+    //List that stores all passwords
+    List<int> Pass = new ArrayList<int>();
+    
+    //List that stores all input passwords
+    List<int> UserPass = new ArrayList<int>();
+    
+    //Asks for pass and checks if the passwords are the same
+    while (Passleft != -1) {
+      Scanner reader = new Scanner(System.in);
+      System.out.println("Enter pass " + PassLeft+1 " :");
+      UserPass.add(reader.nextInt());
+      
+      if (Pass.get(Num) == UserPass.get(Num) { //Checks if you filled in the right pass
+        PassLeft = PassLeft - 1; //One step closer to authentication
+        Num = Num + 0; //Next block
+      };
+      else {
+        if (Num = 1 || Num = 2 && TriesLeft = 1) { //Checks if you have the right to try again
+          TriesLeft = TriesLeft - 1; //Only one try-again
+        };
+        else {
+          PassLeft = Passleft + 1; //One step further away from authentication
+        }
+      };
+    }; //while 
+  }; //function
+  private void main() { //Checks if you are succesfull, most important function in the program
+    if (PassLeft == -1) { //Checks if you are done
+      System.out.println("Authentication succesfull");
+    };
+  }; //function
+}; //class
